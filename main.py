38ac26766ef3d2925bb5560854efb234d2ee5227
Sunhaloo <azmaan151003@gmail.com>
2024-11-23 14:26:50 +0400

    I will continue later on!

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ def print_dashed_line():
 
 # function to display options to user
 def display_options():
-    print("\nOption [1]: Download Audio")
+    print("\nYouTube Video Downloader and Converter\n")
+
+    # display available options to user
+    print("Option [1]: Download Audio")
     print("Option [2]: Download Video")
     print("Option [x]: Exit")
 
@@ -32,10 +35,15 @@ def create_folder(user_option: str):
 
         # check if the path exists
         if os.path.exists(dir_path):
-            print("\nOutput Directory / Folder Already Exists!\n")
+            print_dashed_line()
+            print("Output Directory / Folder Already Exists!")
+            print_dashed_line()
+
         # if the directory / folder does not exists
         else:
-            print("\nCreating Output Folder\n")
+            print_dashed_line()
+            print("Creating Output Folder")
+            print_dashed_line()
             # create the directory / folder
             os.mkdir(os.path.expanduser("~/Desktop/downloaded_audio"))
 
@@ -45,22 +53,78 @@ def create_folder(user_option: str):
 
         # check if the path exists
         if os.path.exists(dir_path):
-            print("\nOutput Directory / Folder Already Exists!\n")
+            print_dashed_line()
+            print("Output Directory / Folder Already Exists!")
+            print_dashed_line()
+
         # if the directory / folder does not exists
         else:
-            print("\nCreating Output Folder\n")
+            print_dashed_line()
+            print("Creating Output Folder")
+            print_dashed_line()
             # create the directory / folder
             os.mkdir(os.path.expanduser("~/Desktop/downloaded_video"))
 
 
-
-# function to download the audio itself
-# WARNING: This is a test only
+# function to allow the user to convert YouTube videos to audio
+# NOTE: for now the downloader will be very simple
 def audio_downloader():
-    pass
+    print("==> Audio Converter Selected\n")
+
+    # display options to the user
+    print("Option [1]: Enter Single YouTube Video")
+    print("Option [2]: Use List of URLs from a Text File")
+    print("Option [3]: Exit")
+    print_dashed_line()
+
+    # ask the user to enter an option
+    user_option = input("Please Select an Option: ")
+
+    # evaluate user's option
+    if user_option == "1":
+        # user wants to only convert 1 YouTube video
+        pass
+
+    elif user_option == "2":
+        # user wants to convert a list of URLs with text file
+        '''
+        WARNING:
+            I don't really know how to do this correctly. Hence, here are some conditions:
+                1. Text File **need** to have a name of 'yt_urls.txt'
+                2. Text File **need** to be located at '~/Desktop'
+        '''
+        text_file = os.path.expanduser("~/Desktop/yt_urls.txt")
+
+        # before we continue with anything... check if file 'yt_urls.txt' exists
+        if os.path.isfile(text_file) and os.path.exists(text_file):
+            print("Good")
+
+        else:
+            print_dashed_line()
+            print("You have no 'yt_urls.txt' File!!!")
+            print_dashed_line()
 
 
-# function to download the video itself
+
+
+
+    elif user_option == "3":
+        # user is closing the program
+        print_dashed_line()
+        print("Good Bye!")
+        print_dashed_line()
+        exit(0)
+
+    # if user does not select something appropriate
+    else:
+        print_dashed_line()
+        print('Wrong Option')
+        print_dashed_line()
+
+
+
+# function to download the YouTube video itself
+# NOTE: for now the downloader will be very simple
 def video_downloader():
     pass
 
@@ -82,7 +146,9 @@ def evaluate_choice(user_choice: str):
 
     elif user_choice == "x":
         # user is closing the program
-        print("\nGood Bye!\n")
+        print_dashed_line()
+        print("Good Bye!")
+        print_dashed_line()
         exit(0)
 
     # if user does not select something appropriate
@@ -98,10 +164,11 @@ def main():
     display_options()
 
     # ask the user to enter his choice
-    user_option = input("Please Select an Option: ")
+    # user_option = input("Please Select an Option: ")
+    audio_downloader()
 
     # call the function to evaluate the user's choice
-    evaluate_choice(user_option)
+    # evaluate_choice(user_option)
 
 
 
---
+++ todo.md
@@ -0,0 +1,3 @@
+# TODO
+
+- [ ] Create a function to check if 'yt_urls.txt' file contains any contents
